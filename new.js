4a829b51da45c929d58379ce53473468e428a7f4
talhakamran786 <70715651+talhakamran786@users.noreply.github.com>
2020-09-11 10:18:56 -0700

    Add files via upload
    Add My Two Files

---
+++ new.js
@@ -0,0 +1,127 @@
+// var comment="My name is Talha";
+// var Data1=200;
+// var Data2=100;
+// var result=Data1 + Data2;
+
+
+
+
+// var Data5=600;
+// var Data6=400;
+// var result2=Data5-Data6;
+
+
+// var Data7=600;
+// var Data8=4;
+// var result3=Data7/Data8;
+
+
+// var Data9=2500;
+// var Data10=120;
+// var result4=Data9+Data10;
+
+
+// var Data11=3000;
+// var Data12=1000;
+// var result5=Data11+Data12-120*13/1+100-1200-134;
+
+// var Data13=660;
+// var Data14=560;
+// var result6=Data13+Data14;
+
+
+
+
+// document.write(result)
+// document.write(result2)
+// document.write(result3)
+// document.write(result4)
+// document.write(result5)
+// document.write(result6)
+
+
+
+// console.log(result)
+// console.log(result2)
+// console.log(result3)
+// console.log(result4)
+// console.log(result5)
+// console.log(result6)
+
+
+// var Data1 = 5;
+
+
+// var Result = Data1+1
+
+// console.log(Data1)
+
+// Data1 = Data1 + 1 
+// console.log(Data1)
+// console.log("END DATA1")
+
+// var Data2 = 5;
+
+// console.log(Data2)
+
+// Data2++
+
+// console.log(Data2)
+
+
+// Data2++
+
+// Data2++
+
+// Data2++
+
+
+// Data2--
+
+
+var LuckyNum = "Talha";
+alert("Your Name is"+" "+LuckyNum)
+
+
+var Passnum = "45574986";
+alert("Your Password is"+" "+Passnum)
+
+
+var Emailword = "Talhakamran";
+alert("Your Name is"+" "+Emailword)
+
+var wordnum = "Talha123456";
+alert("Your Name is"+" "+wordnum)
+
+
+var hello = "46746talha";
+alert("Your Name is"+" "+hello)
+
+
+var kamran= "push";
+alert("Your Name is"+" "+kamran)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
